ba6c1a543f8f537126f86c8be841a636005ba2b1
Masi <51009658+MasiMasinga@users.noreply.github.com>
2022-11-08 11:33:52 +0200

    ci changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23f25ab00a979975"; }
+/******/ 		__webpack_require__.h = function() { return "3a809cffab57d091"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
